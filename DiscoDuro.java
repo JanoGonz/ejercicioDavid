ed3976708682cab717858680b08e6fe60ab34866
David Martínez Bardón <davidmb60@gmail.com>
2019-02-06 13:33:22 +0100

    Commit 07

--- DiscoDuro.java
+++ DiscoDuro.java
@@ -18,19 +18,49 @@ public class DiscoDuro
     {
         sistemas = new ArrayList<SistemaOperativo>();
     }
-    
+
     public void agregarSistema(String nombre, double version, int day, int month, int year, boolean estable) {
         SistemaOperativo sistemaActual = new SistemaOperativo(nombre, version, day, month, year, estable, numeroIdentificativo);
         sistemas.add(sistemaActual);
         numeroIdentificativo++;
     }
-    
+
     public String mostrarSistemas() {
         int contador = 0;
         String sistemasAMostrar = "";
         while (contador != sistemas.size()) {
             sistemasAMostrar += sistemas.get(contador).todasLasCaracteristicas();
             contador++;
+        }
+        return sistemasAMostrar;
+    }
+
+    /** El usuario introduce el nombre de los sistemas a clasificar (Windows, Ubuntu, Mac...)
+     */
+    public String clasificacionPorVersion(String sistema) {
+        String sistemasAMostrar = "";
+        ArrayList<SistemaOperativo> sistemasBackup = new ArrayList<SistemaOperativo>();
+        int contador = 0;
+        for (SistemaOperativo sistemaActual : sistemas) {
+            if (sistemaActual.getNombre().equals(sistema)) {
+                sistemasBackup.add(sistemaActual);
+            }
+        }
+        while (contador < sistemasBackup.size()) {
+            int contador2 = contador + 1;
+            while (contador2 < sistemasBackup.size()) {
+                if (sistemasBackup.get(contador).getVersion() > sistemasBackup.get(contador2).getVersion()) {
+                    SistemaOperativo sistemaBackupTemp = null;
+                    sistemaBackupTemp = sistemasBackup.get(contador);
+                    sistemasBackup.set(contador, sistemasBackup.get(contador2));
+                    sistemasBackup.set(contador2, sistemaBackupTemp);
+                }
+                contador2++;
+            }
+            contador++;
+        }
+        for (SistemaOperativo sistemaActual : sistemasBackup) {
+            sistemasAMostrar += sistemaActual.getNombre() + sistemaActual.getVersion();
         }
         return sistemasAMostrar;
     }
